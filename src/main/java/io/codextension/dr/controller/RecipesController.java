5138481c7ce2c9d868bc15fd71804b4172ea3540
Elie Khoury <elie.kh@gmail.com>
2020-09-17 12:03:39 +0200

    added more functions

--- src/main/java/io/codextension/dr/controller/RecipesController.java
+++ src/main/java/io/codextension/dr/controller/RecipesController.java
@@ -8,8 +8,10 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.codextension.dr.model.Recipe;
@@ -22,7 +24,7 @@ public class RecipesController {
     @Autowired
     private RecipeRepository recipeRepository;
 
-    @GetMapping(value = "/recipes/{memberId}", produces = "application/json", consumes = "application/json")
+    @GetMapping(value = "/recipes/chef/{memberId}", produces = "application/json", consumes = "application/json")
     public List<Recipe> getAllRecipesForMember(@NotNull @PathVariable(required = true) String memberId)
             throws NotFoundException {
         List<Recipe> allRecipes = recipeRepository.findRecipesByFamilyMember(memberId);
@@ -32,7 +34,20 @@ public class RecipesController {
         return allRecipes;
     }
 
-    @GetMapping(value = "/recipes/{memberId}/{recipeId}", produces = "application/json", consumes = "application/json")
+    @DeleteMapping(value = "/recipes/{recipeId}/chef/{memberId}", produces = "application/json", consumes = "application/json")
+    public boolean deleteRecipeForMember(@NotNull @PathVariable(required = true) String memberId,
+            @NotNull @PathVariable(required = true) Long recipeId) throws NotFoundException {
+        // delete by id and owner
+        return true;
+    }
+
+    @PutMapping(value = "/recipes/{recipeId}/chef/{memberId}", produces = "application/json", consumes = "application/json")
+    public Recipe updateRecipeForMember(@NotNull @PathVariable(required = true) String memberId,
+            @NotNull @PathVariable(required = true) Long recipeId) throws NotFoundException {
+        return null;
+    }
+
+    @GetMapping(value = "/recipes/{recipeId}/chef/{memberId}", produces = "application/json", consumes = "application/json")
     public Recipe getRecipeForMember(@NotNull @PathVariable(required = true) String memberId,
             @NotNull @PathVariable(required = true) Long recipeId) throws NotFoundException {
         Optional<Recipe> recipe = recipeRepository.findRecipeByFamilyMember(memberId, recipeId);
